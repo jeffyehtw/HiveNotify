4bbddc589f13a8affa6e065ded49606ae37644d7
yehcj <yehcj.tw@gmail.com>
2022-07-03 17:04:46 +0800

    fix key path

--- heartbeat.py
+++ heartbeat.py
@@ -8,6 +8,7 @@ from datetime import datetime
 
 __status__ = 'http://{ip}:{port}/api/v1/status'
 __sheet__ = 'https://docs.google.com/spreadsheets/d/{id}/edit?usp=sharing'
+__path__ = os.path.dirname(os.path.realpath(__file__))
 
 def main():
     parser = argparse.ArgumentParser()
@@ -15,7 +16,7 @@ def main():
     parser.add_argument('cell', help='')
     parser.add_argument('--ip', default='127.0.0.1', help='')
     parser.add_argument('--port', default=22333, help='')
-    parser.add_argument('--key', default='key.json', help='')
+    parser.add_argument('--key', default=os.path.join(__path__, 'key.json'), help='')
     parser.add_argument('--sheet', default='1O34xZfhJdFeZd-s4M4bIOhdNhbyFw7M7y4nzoHyknwQ', help='')
 
     args = parser.parse_args()
