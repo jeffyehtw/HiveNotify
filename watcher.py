db515cfb5b0197b41de12efadbe31ae9e289c415
yehcj <yehcj.tw@gmail.com>
2022-07-30 00:33:54 +0800

    add outlet

--- watcher.py
+++ watcher.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import json
+import time
+import tinytuya
 import argparse
 import requests
 import pygsheets
@@ -16,11 +18,35 @@ def notify(bearer, message):
         message=message
     ))
 
+def outlet(auth, device, val):
+    cloud = tinytuya.Cloud(
+        apiRegion="us",
+        apiKey=auth['key'],
+        apiSecret=auth['secret'],
+        apiDeviceID=auth['device_id'])
+
+    commands = {
+        'commands': [
+            {
+                'code': 'switch_1',
+                'value': val
+            },
+            {
+                'code': 'countdown_1',
+                'value': 0
+            }
+        ]
+    }
+
+    result = cloud.sendcommand(device, commands)
+    return result['success'] == True
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('bearer', help='')
     parser.add_argument('--key', default=os.path.join(__path__, 'key.json'), help='')
     parser.add_argument('--sheet', default='1O34xZfhJdFeZd-s4M4bIOhdNhbyFw7M7y4nzoHyknwQ', help='')
+    parser.add_argument('--tuya', nargs='+', help='')
     args = parser.parse_args()
 
     certificate = pygsheets.authorize(service_file=args.key)
@@ -37,11 +63,37 @@ def main():
             current = datetime.now()
             time_delta = current - report
 
-            if current - report > timedelta(minutes=25):
-                notify(args.bearer, '{farm}.{worker} is offline'.format(
-                    farm=wks.title,
-                    worker=worker['miner']
-                ))
+            if time_delta > timedelta(minutes=25):
+                result = True
+
+                if worker['outlet'] == '':
+                    result = False
+                else:
+                    result &= outlet(
+                        {
+                            'key': args.tuya[0],
+                            'secret': args.tuya[1],
+                            'device_id': args.tuya[2]
+                        },
+                        worker['outlet'],
+                        False
+                    )
+                    time.sleep(5)
+                    result &= outlet(
+                        {
+                            'key': args.tuya[0],
+                            'secret': args.tuya[1],
+                            'device_id': args.tuya[2]
+                        },
+                        worker['outlet'],
+                        True
+                    )
+
+                if result == False:
+                    notify(args.bearer, '{farm}.{worker} is offline'.format(
+                        farm=wks.title,
+                        worker=worker['miner']
+                    ))
 
 if __name__ == '__main__':
     main()
